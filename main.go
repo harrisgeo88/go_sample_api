7605bddbf730c661764c05d7bfb924e1e4a14455
Harris Geo <charilaoss@gmail.com>
2017-09-27 11:09:30 +0100

    basic get api functionality added

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"log"
+	"encoding/json"
+)
+
+type Person struct {
+	ID string `json:"id,omitempty"`
+	Firstname string `json:"firstname,omitempty"`
+	Lastname string `json:"lastname,omitempty"`
+	Address *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+
+var people []Person
+
+func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(people)
+
+}
+
+func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+
+func main() {
+	router := mux.NewRouter()
+
+
+	people = append(people, Person{ID: "1", Firstname: "Harris", Lastname:"Geo", Address: &Address{City: "London", State: "Greater London"}})
+	people = append(people, Person{ID: "2", Firstname: "Vasileios", Lastname:"Geo"})
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	log.Fatal(http.ListenAndServe(":12345", router))
+
+}
